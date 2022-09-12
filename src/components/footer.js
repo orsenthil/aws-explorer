fae5a0b875964d74c6a04a16a71b54b873867dd2
Senthil Kumaran <senthilx@amazon.com>
2022-09-11 21:21:30 -0700

    Updated footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,36 +4,36 @@ import github from 'assets/images/github.svg';
 import githubGreen from 'assets/images/github-green.svg';
 
 const Footer = props => (
-  <footer className="footer">
+    <footer className="footer">
     <div className="logo">
-      <a href="https://github.com/orsenthil/aws-explorer" rel="noopener noreferrer" target="_blank">
-        {props.dark ? (
-          <img src={githubGreen} alt="Github Logo" className="logo--github" />
-        ) : (
-          <img src={github} alt="Github Logo" className="logo--github" />
-        )}
-      </a>
+    <a href="https://github.com/orsenthil/aws-explorer" rel="noopener noreferrer" target="_blank">
+    {props.dark ? (
+        <img src={githubGreen} alt="Github Logo" className="logo--github" />
+    ) : (
+        <img src={github} alt="Github Logo" className="logo--github" />
+    )}
+    </a>
     </div>
     <p className="footer__copyright dark-white">
-      Made with <span>❤</span> by{' '}
-      From <a href="https://senthil.learntosolveit.com" target="_blank" rel="noopener noreferrer">
+    Made with <span>❤</span> by{' '}
+    <a href="https://senthil.learntosolveit.com" target="_blank" rel="noopener noreferrer">
     Senthil Kumaran.</a>
     See also <span role="img" aria-label="pointer">👉</span>{' '}
-      <a href="https://eksctl.learntosolveit.com/" target="_blank" rel="noopener noreferrer">eksctl-explorer,</a>
-      <a href="https://kubectl.learntosolveit.com/" target="_blank" rel="noopener noreferrer">kubectl-explorer</a>
+    <a href="https://eksctl.learntosolveit.com/" target="_blank" rel="noopener noreferrer">eksctl-explorer,</a>
+    <a href="https://kubectl.learntosolveit.com/" target="_blank" rel="noopener noreferrer">kubectl-explorer</a>
     </p>
 
     <p className="footer__copyright dark-white">
-      <span role="img" aria-label="thank you">🙏</span> Thanks to parent projects {' '}
-      <a href="https://gitexplorer.com/" target="_blank" rel="noopener noreferrer"> GitExplorer</a>, 
-      <a href="https://objectexplorer.netlify.app/" target="_blank" rel="noopener noreferrer"> Javascript Object Explorer.</a>
+    <span role="img" aria-label="thank you">🙏</span> Thanks to parent projects {' '}
+    <a href="https://gitexplorer.com/" target="_blank" rel="noopener noreferrer"> GitExplorer</a>,
+    <a href="https://objectexplorer.netlify.app/" target="_blank" rel="noopener noreferrer"> Javascript Object Explorer.</a>
     </p>
 
-  </footer>
+    </footer>
 );
 
 Footer.propTypes = {
-  dark: PropTypes.bool
+    dark: PropTypes.bool
 };
 
 export { Footer };
