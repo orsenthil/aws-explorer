b7d94e0b95ed2954967ef9f615cf7428f1fe4496
Senthil Kumaran <senthilx@amazon.com>
2022-09-19 07:15:02 -0700

    Secondary Options.

--- src/data/secondary-options.js
+++ src/data/secondary-options.js
@@ -4,5 +4,11 @@ export const secondaryOptions = {
             value: 'list-clusters',
             label: 'list clusters',
         },
+        {
+            value: 'attach-cluster-to-kubeconfig',
+            label: 'attach cluster to kubeconfig',
+            usage: 'aws eks update-kubeconfig --name $CLUSTER_NAME',
+            nb: 'cluster-name is the name of the cluster you want to attach to your kubeconfig',
+        },
     ],
 };
